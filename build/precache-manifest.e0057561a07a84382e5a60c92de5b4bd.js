9f882402408da4938e33b57e198216b3bdf7055e
Ayodapo Adesiyan <48982301+dapslegend@users.noreply.github.com>
2022-12-30 17:52:15 +0100

    Updates
    updates

---
+++ build/precache-manifest.e0057561a07a84382e5a60c92de5b4bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ef2499fae406a6eacb9e539e3364350",
+    "url": "/Adesiyan_Ayodapo/index.html"
+  },
+  {
+    "revision": "d908fb6253aa71f96732",
+    "url": "/Adesiyan_Ayodapo/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "1341ce6ac7c420079635",
+    "url": "/Adesiyan_Ayodapo/static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "d908fb6253aa71f96732",
+    "url": "/Adesiyan_Ayodapo/static/js/2.8b9fcb0d.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/Adesiyan_Ayodapo/static/js/2.8b9fcb0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1341ce6ac7c420079635",
+    "url": "/Adesiyan_Ayodapo/static/js/main.6f81f6f1.chunk.js"
+  },
+  {
+    "revision": "78c6fd8570cb4df3c826",
+    "url": "/Adesiyan_Ayodapo/static/js/runtime-main.1f79b4db.js"
+  }
+]);
